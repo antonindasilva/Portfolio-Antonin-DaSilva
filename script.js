a12688cc7abeea2a80aeaa04587ba6d9e78077ad
antonindasilva <antonin.dasilva69@gmail.com>
2026-05-13 16:54:37 +0200

    v3

---
+++ script.js
@@ -0,0 +1,17 @@
+//Je recup le header
+let header = document.querySelector("header")
+//Je recup le hero
+let hero = document.querySelector(".bg-hero2")
+// j'ecoute le scroll:
+window.addEventListener("scroll",function(){
+    
+
+    //si l'utilisateur a scrollé plus bas que le hero:
+    if(window.scrollY > hero.clientHeight){
+        header.classList.add("bg-bleu")
+    // je donne au header une classe qui change le bg
+    }else{
+        header.classList.remove("bg-bleu")
+
+    }
+})    
